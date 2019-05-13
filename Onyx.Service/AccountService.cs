6ca1cc59b62d8235fa2df43e9055df487ed8315c
kevin <kl.300155@gmail.com>
2019-05-13 16:57:48 -0300

    a

---
+++ Onyx.Service/AccountService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Onyx.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+namespace Onyx.Service
+{
+    public partial class AccountService
+    {
+        public void SaveUser(Usuario usuario)
+        {
+            var objectContext = new OnyxEntities();
+            objectContext.Usuarios.Add(usuario);
+            objectContext.SaveChanges();
+        }
+    }
+}
---
+++ Onyx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Onyx.Startup))]
+namespace Onyx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
